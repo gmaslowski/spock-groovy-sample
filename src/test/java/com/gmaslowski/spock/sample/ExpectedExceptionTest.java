fe0f116b36021f2cc251e182d7266cbf8f0fb852
gmaslowski <gregmaslowski@gmail.com>
2015-09-28 09:12:27 +0200

    Trying to be consistent.

--- src/test/java/com/gmaslowski/spock/sample/ExpectedExceptionTest.java
+++ src/test/java/com/gmaslowski/spock/sample/ExpectedExceptionTest.java
@@ -3,6 +3,7 @@ package com.gmaslowski.spock.sample;
 import com.gmaslowski.spock.sample.exception.DummyNotFoundException;
 import com.gmaslowski.spock.sample.repo.DummyRepository;
 import com.gmaslowski.spock.sample.service.DummyService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.fest.assertions.Assertions.assertThat;
 import static org.junit.rules.ExpectedException.none;
 
 public class ExpectedExceptionTest {
@@ -32,7 +34,7 @@ public class ExpectedExceptionTest {
     private DummyService dummyService;
 
     @Test
-    public void shouldNotDeactivateDummyWithNegativeId() {
+    public void shouldNotDeactivateDummyWithNegativeIdUsingRuleStyle() {
         // then
         expectedException.expect(DummyNotFoundException.class);
         expectedException.expectMessage(String.format("Could not find dummy with id %d", NEGATIVE_ID));
@@ -41,4 +43,28 @@ public class ExpectedExceptionTest {
         dummyService.dummyLogic(NEGATIVE_ID);
     }
 
+    @Test
+    public void shouldNotDeactivateDummyWithNegativeIdUsingTryCatchStyle() {
+        // when
+        DummyNotFoundException caughtException = null;
+        try {
+            dummyService.dummyLogic(NEGATIVE_ID);
+            Assert.fail("Expected dummy not to be found");
+        } catch (DummyNotFoundException e) {
+            caughtException = e;
+        }
+
+        // then
+        assertThat(caughtException)
+                .isNotNull()
+                .hasMessage("Could not find dummy with id " + NEGATIVE_ID);
+    }
+
+    @Test(expected = DummyNotFoundException.class)
+    public void shouldNotDeactivateDummyWithNegativeIdUsingTestLevelStyle() {
+        // when
+        dummyService.dummyLogic(NEGATIVE_ID);
+
+        // then, an exception is thrown
+    }
 }
